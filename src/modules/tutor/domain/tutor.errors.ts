ecadda59c4a2ba7a3fd0600f6c36a8fe582a2caf
Thiago Alves <107894642+Thiagoalves033@users.noreply.github.com>
2024-07-23 18:40:34 -0300

    feat: add TutorAlreadyExists domain exception

---
+++ src/modules/tutor/domain/tutor.errors.ts
@@ -0,0 +1,10 @@
+import { ExceptionBase } from '../../../libs/exceptions';
+
+export class TutorAlreadyExistsException extends ExceptionBase {
+  static readonly message = 'Tutor already exists';
+  readonly code = 'TUTOR_ALREADY_EXISTS';
+
+  constructor(readonly cause?: Error) {
+    super(TutorAlreadyExistsException.message, cause);
+  }
+}
