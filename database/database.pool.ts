f5d3897e7124443cc482ab7fe7c3825b843eb1dc
Thiago Alves <107894642+Thiagoalves033@users.noreply.github.com>
2024-07-23 19:19:48 -0300

    feat: add database pool

---
+++ database/database.pool.ts
@@ -0,0 +1,7 @@
+import { createPool, DatabasePool } from 'slonik';
+
+export async function getDatabasePool(): Promise<DatabasePool> {
+  return await createPool(
+    `postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}`
+  );
+}
