e6f61e300053c8d2f988aecbfe51294328dcb5c1
Thiago Alves <107894642+Thiagoalves033@users.noreply.github.com>
2024-07-23 18:37:00 -0300

    feat: add TutorRepository class

---
+++ src/modules/tutor/storage/tutor.repository.ts
@@ -0,0 +1,24 @@
+import z from 'zod';
+import { SqlRepositoryBase } from '../../../libs/db/sql-repository.base';
+import { TutorEntity } from '../domain/tutor.entity';
+import { DatabasePool } from 'slonik';
+
+export const tutorSchema = z.object({
+  id: z.string().uuid(),
+  createdAt: z.preprocess((val: any) => new Date(val), z.date()),
+  updatedAt: z.preprocess((val: any) => new Date(val), z.date()),
+  name: z.string().min(1).max(70),
+  phone_number: z.string().min(1).max(15),
+  email: z.string().min(1).max(256).email(),
+  date_of_birth: z.date(),
+  zip_code: z.string().min(1).max(15)
+});
+
+export class TutorRepository extends SqlRepositoryBase<TutorEntity> {
+  protected tableName = 'tutors';
+  protected schema = tutorSchema;
+
+  constructor(pool: DatabasePool) {
+    super(pool);
+  }
+}
