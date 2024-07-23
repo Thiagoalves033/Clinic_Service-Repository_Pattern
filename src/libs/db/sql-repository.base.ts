378f48d406a4d6c124eea44f9ef06f7eb456cbad
Thiago Alves <107894642+Thiagoalves033@users.noreply.github.com>
2024-07-23 18:29:44 -0300

    feat: add abstract SqlRepositoryBase class

---
+++ src/libs/db/sql-repository.base.ts
@@ -0,0 +1,82 @@
+import {
+  DatabasePool,
+  IdentifierSqlToken,
+  sql,
+  UniqueIntegrityConstraintViolationError
+} from 'slonik';
+import { RepositoryPort } from '../ports/repository.port';
+import { BaseEntity } from '../utils/entity.base';
+import { ZodObject } from 'zod';
+import { ConflictException } from '../exceptions';
+
+export abstract class SqlRepositoryBase<Entity extends BaseEntity<any>>
+  implements RepositoryPort<Entity>
+{
+  protected abstract tableName: string;
+  protected abstract schema: ZodObject<any>;
+
+  protected constructor(private readonly pool: DatabasePool) {}
+
+  async insert(entity: Entity): Promise<void> {
+    const propertyNames: IdentifierSqlToken[] = [];
+    const values: any = [];
+
+    const entries = Object.entries(entity).flatMap(([key, value]) => {
+      if (typeof value === 'object' && value !== null && !(value instanceof Date)) {
+        return Object.entries(value).map(([propKey, propValue]) => [propKey, propValue]);
+      }
+
+      return [[key, value]];
+    });
+
+    entries.forEach((entry) => {
+      if (entry[0] && entry[1] !== undefined) {
+        propertyNames.push(sql.identifier([entry[0]]));
+
+        if (entry[1] instanceof Date) {
+          values.push(sql.timestamp(entry[1]));
+        } else {
+          values.push(sql.literalValue(entry[1]));
+        }
+      }
+    });
+
+    const query = sql.type(this.schema)`INSERT INTO ${sql.identifier([this.tableName])} 
+      (${sql.join(propertyNames, sql.fragment`, `)}) VALUES 
+      (${sql.join(values, sql.fragment`, `)})`;
+
+    try {
+      await this.pool.query(query);
+    } catch (error) {
+      if (error instanceof UniqueIntegrityConstraintViolationError) {
+        throw new ConflictException('Record already exists', error);
+      }
+
+      throw error;
+    }
+  }
+
+  async findOneById(id: string): Promise<Entity | null> {
+    const query = sql.type(
+      this.schema
+    )`SELECT * FROM ${sql.identifier([this.tableName])} WHERE id = ${id}`;
+
+    const result = await this.pool.query(query);
+    return result.rows[0] ? (result.rows[0] as Entity) : null;
+  }
+
+  async findAll(): Promise<Entity[]> {
+    const query = sql.type(this.schema)`SELECT * FROM ${sql.identifier([this.tableName])}`;
+
+    const result = await this.pool.query(query);
+    return result.rows as Entity[];
+  }
+
+  async deleteById(id: string): Promise<void> {
+    const query = sql.type(
+      this.schema
+    )`DELETE * FROM ${sql.identifier([this.tableName])} WHERE id = ${id}`;
+
+    await this.pool.query(query);
+  }
+}
