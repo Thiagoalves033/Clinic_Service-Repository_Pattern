e396b96d8e9dcb537c2f0f05d18976a9e3956492
Thiago Alves <107894642+Thiagoalves033@users.noreply.github.com>
2024-08-01 21:21:18 -0300

    feat: modify tutor entity to comply to recent changes
    This commits makes changes to TutorEntity class and types, adding the
    use of value objects and PetEntity.

--- src/modules/tutor/domain/tutor.entity.ts
+++ src/modules/tutor/domain/tutor.entity.ts
@@ -1,117 +1,78 @@
-import {
-  ArgumentInvalidException,
-  ArgumentNotProvidedException,
-  ArgumentOutOfRangeException
-} from '@libs/exceptions';
 import { BaseEntity } from '@libs/utils/entity.base';
 import { randomUUID } from 'crypto';
-import { ChangeTutorInformationProps, CreateTutorProps } from './tutor.types';
+import { PetEntity } from './pet/pet.entity';
+import { CreatePetProps } from './pet/pet.types';
+import { Pets } from './pet/pets.list';
+import {
+  CreateTutorProps,
+  TutorProps,
+  UpdateTutorAddressProps,
+  UpdateTutorContactProps,
+  UpdateTutorNameProps
+} from './tutor.types';
+import { Address, AddressProps, Contact, ContactProps, Name } from './value-objects';
 
-export class TutorEntity extends BaseEntity<CreateTutorProps> {
+export class TutorEntity extends BaseEntity<TutorProps> {
   protected readonly id: string;
 
-  constructor(id: string, props: CreateTutorProps) {
+  constructor(id: string, props: TutorProps) {
     super({ id: id, props: props });
     this.id = id;
   }
 
   public static create(create: CreateTutorProps): TutorEntity {
     const id = randomUUID();
-
-    TutorEntity.ValidateName(create.name);
-    TutorEntity.ValidatePhoneNumber(create.phone_number);
-    TutorEntity.ValidateEmail(create.email);
-    TutorEntity.ValidateDateOfBirth(create.date_of_birth);
-    TutorEntity.ValidateZipCode(create.zip_code);
-
-    return new TutorEntity(id, create);
+    const props = { ...create, pets: Pets.create() };
+    return new TutorEntity(id, props);
   }
 
-  private ChangeTutorInformation(update: ChangeTutorInformationProps): void {
-    if (update.name) {
-      TutorEntity.ValidateName(update.name);
-      this.props.name = update.name;
-    }
-
-    if (update.phone_number) {
-      TutorEntity.ValidatePhoneNumber(update.phone_number);
-      this.props.phone_number = update.phone_number;
-    }
-
-    if (update.email) {
-      TutorEntity.ValidateEmail(update.email);
-      this.props.email = update.email;
-    }
-
-    if (update.zip_code) {
-      TutorEntity.ValidateZipCode(update.zip_code);
-      this.props.zip_code = update.zip_code;
-    }
+  public updateTutorName(update: UpdateTutorNameProps): void {
+    this.props.name = new Name(update);
+    this.setUpdatedAt();
   }
 
-  private static ValidateName(name: string): void {
-    if (name == null) {
-      throw new ArgumentNotProvidedException('Name cannot be empty');
-    }
+  public updateTutorContact(update: UpdateTutorContactProps): void {
+    const newTutorContact = new Contact({
+      ...this.props.contact.getContact(),
+      ...update
+    } as ContactProps);
 
-    if (name.length > 70) {
-      throw new ArgumentOutOfRangeException('Name cannot be over 70 characters');
-    }
+    this.props.contact = newTutorContact;
+    this.setUpdatedAt();
   }
 
-  private static ValidatePhoneNumber(phone_number: string): void {
-    if (phone_number == null) {
-      throw new ArgumentNotProvidedException('Phone Number cannot be empty');
-    }
+  public updateTutorAddress(update: UpdateTutorAddressProps): void {
+    const newTutorAddress = new Address({
+      ...this.props.address.getAddress(),
+      ...update
+    } as AddressProps);
 
-    if (phone_number.length > 15) {
-      throw new ArgumentOutOfRangeException('Phone Number cannot be over 15 characters');
-    }
-
-    if (isNaN(Number(phone_number))) {
-      throw new ArgumentInvalidException('Phone Number provided is not valid');
-    }
+    this.props.address = newTutorAddress;
+    this.setUpdatedAt();
   }
 
-  private static ValidateEmail(email: string): void {
-    if (email == null) {
-      throw new ArgumentNotProvidedException('Email cannot be empty');
-    }
-
-    if (email.length > 256) {
-      throw new ArgumentOutOfRangeException('Email cannot be over 256 characters');
-    }
-
-    if (
-      !/^(([^<>()[\]\\.,;:\s@]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(.+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.exec(
-        email
-      )
-    ) {
-      throw new ArgumentInvalidException('Email provided is not valid');
-    }
+  getName() {
+    return this.props.name.getName();
   }
 
-  private static ValidateZipCode(zip_code: string): void {
-    if (zip_code == null) {
-      throw new ArgumentNotProvidedException('Zip Code cannot be empty');
-    }
-
-    if (zip_code.length > 15) {
-      throw new ArgumentOutOfRangeException('Zip Code cannot be over 15 characters');
-    }
-
-    if (!/^[a-z0-9]+$/i.exec(zip_code)) {
-      throw new ArgumentInvalidException('Zip Code provided is not valid');
-    }
+  public getContact() {
+    return this.props.contact.getContact();
   }
 
-  private static ValidateDateOfBirth(date_of_birth: Date): void {
-    if (date_of_birth == null) {
-      throw new ArgumentNotProvidedException('Date cannot be empty');
-    }
+  public getAddress() {
+    return this.props.address.getAddress();
+  }
 
-    if (new Date(date_of_birth) > new Date(Date.now())) {
-      throw new ArgumentInvalidException('Date provided is not valid');
-    }
+  public getPets() {
+    return this.props.pets.getItems();
+  }
+
+  public addPet(pet: CreatePetProps): void {
+    const newPet = PetEntity.create(pet);
+    this.props.pets.add(newPet);
+  }
+
+  public removePet(pet: PetEntity): void {
+    this.props.pets.remove(pet);
   }
 }
--- src/modules/tutor/domain/tutor.types.ts
+++ src/modules/tutor/domain/tutor.types.ts
@@ -1,14 +1,35 @@
-export interface CreateTutorProps {
-  name: string;
-  phone_number: string;
-  email: string;
-  date_of_birth: Date;
-  zip_code: string;
+import { Pets } from './pet/pets.list';
+import { Address } from './value-objects/address.value-object';
+import { BirthDate } from './value-objects/birth-date.value-object';
+import { Contact } from './value-objects/contact.value-object';
+import { Name } from './value-objects/name.value-object';
+
+export interface TutorProps {
+  name: Name;
+  date_of_birth: BirthDate;
+  contact: Contact;
+  address: Address;
+  pets: Pets;
 }
 
-export interface ChangeTutorInformationProps {
-  name?: string;
-  phone_number?: string;
+export interface CreateTutorProps {
+  name: Name;
+  date_of_birth: BirthDate;
+  contact: Contact;
+  address: Address;
+}
+
+export interface UpdateTutorNameProps {
+  name: string;
+}
+
+export interface UpdateTutorContactProps {
   email?: string;
+  phone_number?: string;
+}
+
+export interface UpdateTutorAddressProps {
+  state?: string;
+  city?: string;
   zip_code?: string;
 }
